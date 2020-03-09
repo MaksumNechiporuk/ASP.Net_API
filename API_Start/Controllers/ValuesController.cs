e18ff2c3318d7ee7adfca13a2159eb22718e5358
MaksumNechiporuk <maksum.nec@gmail.com>
2020-03-09 11:45:50 +0200

    done

--- API_Start/Controllers/ValuesController.cs
+++ API_Start/Controllers/ValuesController.cs
@@ -32,27 +32,42 @@ namespace API_Start.Controllers
         }
 
         // POST api/values
-        public void Post([FromBody]string FirstName,string LastName, string Gender, int Salary)
-        {
+        public void Post(string FirstName, string LastName, string Gender, int Salary)
+        {        
             Employees new_emp = new Employees
             {
-               FirstName=FirstName,
-               LastName=LastName,
-               Salary=Salary,
-               Gender=Gender
+                FirstName = FirstName,
+                LastName = LastName,
+                Salary = Salary,
+                Gender = Gender
             };
             _dbContext.Employees.Add(new_emp);
             _dbContext.SaveChanges();
         }
 
         // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
+        public void Put(int id, string FirstName, string LastName, string Gender, int Salary)
         {
+            var emp = _dbContext.Employees.SingleOrDefault(e => e.Id == id);
+
+
+            emp.FirstName = FirstName;
+            emp.LastName = LastName;
+            emp.Salary = Salary;
+            emp.Gender = Gender;
+            _dbContext.SaveChanges();
         }
 
         // DELETE api/values/5
         public void Delete(int id)
         {
+            var emp = _dbContext.Employees.SingleOrDefault(e => e.Id == id);
+            if (emp != null)
+            {
+                _dbContext.Employees.Remove(emp);
+                _dbContext.SaveChanges();
+            }
+       
         }
     }
 }
